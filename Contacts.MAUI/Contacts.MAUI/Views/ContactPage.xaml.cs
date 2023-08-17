4810f4ffa1a76e1a296f032ba38d81ac65d7ab4e
DoumSof99 <sofiadmn99@gmail.com>
2023-08-17 17:46:55 +0300

    added Backend code for binding

--- Contacts.MAUI/Contacts.MAUI/Views/ContactPage.xaml.cs
+++ Contacts.MAUI/Contacts.MAUI/Views/ContactPage.xaml.cs
@@ -5,15 +5,26 @@ public partial class ContactPage : ContentPage
 	public ContactPage()
 	{
 		InitializeComponent();
-	}
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
+		List<Contact> contacts = new List<Contact>() 
+		{ 
+			new Contact() { Name = "Sofia", Email="SofDoum@email.com" },
+            new Contact() { Name = "Irini", Email="Irini@email.com" },
+            new Contact() { Name = "Maria", Email="Maria@email.com" },
+            new Contact() { Name = "Menia", Email="Menia@email.com" },
+            new Contact() { Name = "Agapi", Email="Agapi@email.com" }
+        };
+		
+        listContacts.ItemsSource = contacts;
+
     }
 
-    private void btnAddContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
+	public class Contact
+	{
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
     }
+   
 }
