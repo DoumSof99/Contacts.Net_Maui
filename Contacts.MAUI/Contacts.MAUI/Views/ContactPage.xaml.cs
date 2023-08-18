4ecb2e2303c235e8b6606819afbbec9588b7491c
DoumSof99 <sofiadmn99@gmail.com>
2023-08-18 14:52:59 +0300

    Events Handling of ListView

--- Contacts.MAUI/Contacts.MAUI/Views/ContactPage.xaml.cs
+++ Contacts.MAUI/Contacts.MAUI/Views/ContactPage.xaml.cs
@@ -26,5 +26,17 @@ public partial class ContactPage : ContentPage
 
 
     }
-   
+
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (listContacts.SelectedItem != null)
+        {
+            await Shell.Current.GoToAsync(nameof(EditContactPage));
+        }
+    }
+
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        listContacts.SelectedItem = null;   
+    }
 }
