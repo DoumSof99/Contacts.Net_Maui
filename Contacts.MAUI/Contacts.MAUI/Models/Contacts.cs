797ff5dc13cca6735c92786bdf8084b4fd0fa6f4
DoumSof99 <sofiadmn99@gmail.com>
2023-08-21 14:57:37 +0300

    Added in Contacts class more props, fix layout of Edit page

--- Contacts.MAUI/Contacts.MAUI/Models/Contacts.cs
+++ Contacts.MAUI/Contacts.MAUI/Models/Contacts.cs
@@ -11,6 +11,8 @@ namespace Contacts.MAUI.Models
         public int ContactId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
 
 
     }
