a8b8efd3339d9bdb53a6bbcccde9dd0729032102
DoumSof99 <sofiadmn99@gmail.com>
2023-08-28 17:09:31 +0300

    Field validation with .Net MAUI CommunityToolkit

--- Contacts.MAUI/Contacts.MAUI/MauiProgram.cs
+++ Contacts.MAUI/Contacts.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Contacts.MAUI
 {
@@ -9,6 +10,7 @@ namespace Contacts.MAUI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,7 +18,7 @@ namespace Contacts.MAUI
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
