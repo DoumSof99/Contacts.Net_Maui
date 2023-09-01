97de094071e738f1790f63b78f32c4ed57b39e70
DoumSof99 <sofiadmn99@gmail.com>
2023-09-01 10:55:02 +0300

    Clean Architecture with Use Case Driven Development

---
+++ Contacts.MAUI/Contacts.UseCases/ViewContactsUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Contacts.UseCases.PluginInterfaces;
+using Contact = Contacts.CoreBusiness.Contact;
+
+namespace Contacts.UseCases
+{
+    // All the code in this file is included in all platforms.
+    public class ViewContactsUseCase
+    {
+        private readonly IContactRepository contactRepositiry;
+        public ViewContactsUseCase(IContactRepository contactRepositiry)
+        {
+            this.contactRepositiry = contactRepositiry;
+        }
+
+        public async Task<List<Contact>> ExecuteAsync(string filterText)
+        {
+            return await this.contactRepositiry.GetContactsAsync(filterText);
+        }
+    }
+}
