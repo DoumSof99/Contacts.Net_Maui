3e3827e33cc4d0c81128a401bcb75426859f2e6e
DoumSof99 <sofiadmn99@gmail.com>
2024-03-20 12:36:18 +0200

    Update contact with MVVM

--- Contacts.MAUI/Contacts.MAUI/ViewModels/ContactViewModel.cs
+++ Contacts.MAUI/Contacts.MAUI/ViewModels/ContactViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Contacts.MAUI.Models;
+using Contacts.MAUI.Views_MVVM;
 using Contacts.UseCases.Interfaces;
 using Contact = Contacts.CoreBusiness.Contact;
 
@@ -13,6 +14,7 @@ namespace Contacts.MAUI.ViewModels {
     public partial class ContactViewModel : ObservableObject {
         private Contact contact;
         private readonly IViewContactUseCase viewContactUseCase;
+        private readonly IEditContactUseCase editContactUseCase;
 
         public Contact Contact {
             get => contact;
@@ -21,19 +23,27 @@ namespace Contacts.MAUI.ViewModels {
             }
         }
 
-        public ContactViewModel(IViewContactUseCase viewContactUseCase)
+        public ContactViewModel(IViewContactUseCase viewContactUseCase,
+                                IEditContactUseCase editContactUseCase)
         {
             this.Contact = new Contact();
             this.viewContactUseCase = viewContactUseCase;
+            this.editContactUseCase = editContactUseCase;
         }
 
         public async Task LoadContact(int contactId) {
             this.Contact = await this.viewContactUseCase.ExecuteAsync(contactId);
         }
 
-        //[RelayCommand]
-        //public void SaveContact() {
-        //    ContactRepository.UpdeteContact(this.Contact.ContactId, this.Contact);
-        //}
+        [RelayCommand]
+        public async Task EditContact() {
+            await this.editContactUseCase.ExecuteAsync(this.contact.ContactId, this.contact);
+            await Shell.Current.GoToAsync($"{nameof(Contacts_MVVM_Page)}");
+        }
+
+        [RelayCommand]
+        public async Task BackToContact() {
+            await Shell.Current.GoToAsync($"{nameof(Contacts_MVVM_Page)}");
+        }
     }
 }
