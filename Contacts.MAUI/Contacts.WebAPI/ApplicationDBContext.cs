b759356ff1b99b7bde4c4268ac6a455f2737eb4a
DoumSof99 <sofiadmn99@gmail.com>
2024-04-29 10:34:41 +0300

    Configure DbContext

---
+++ Contacts.MAUI/Contacts.WebAPI/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Contacts.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.WebAPI {
+    public class ApplicationDBContext : DbContext {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Contact>().HasKey()
+        //}
+
+    }
+}
